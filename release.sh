4b681dc178a3f8e3aee783a75575bf6535087d57
Timo Göckeler <timo.goeckeler@gmail.com>
2022-05-08 14:06:07 +0200

    Install Arkusia in applications folder

--- release.sh
+++ release.sh
@@ -5,8 +5,20 @@ echo Building release of Arkusia
 echo ----------------------------------------------------------------------------------
 
 ./mvnw
-rm -rf Arkusia
-jpackage --type app-image -i target -n Arkusia --main-class de.arkusia.ui.Main --main-jar arkusia-ui-1.0.0.jar
+
+rm -rf ~/Anwendungen/Arkusia
+cp -f target/arkusia-ui-1.0.0.jar release/
+
+jpackage --type app-image \
+         --input release \
+         --name Arkusia \
+         --app-version 1.0.0 \
+         --copyright "(C) 2022 Timo Göckeler" \
+         --description "Adventure Learning Game by Timo Göckeler" \
+         --main-class de.arkusia.ui.Main \
+         --main-jar arkusia-ui-1.0.0.jar \
+         --icon release/arkusia.png \
+         --dest ~/Anwendungen 
 
 echo ----------------------------------------------------------------------------------
 echo Arkusia is ready to be played.
